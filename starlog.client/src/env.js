f3e1686d1e84aafc73c3a00e4ca5cf8a6ad51ef0
Lane <lanericharddavis@gmail.com>
2021-05-27 09:38:48 -0600

    inital push

--- starlog.client/src/env.js
+++ starlog.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'lanerd.us.auth0.com'
+export const audience = 'https://myKanbanApi.com'
+export const clientId = 'MrpO1Gkua5NxFhPG5U28Lksb5xPTWGU7'
